abe1857dbb7ae6afa4eaea3efa8bfd48dfb2a717
Dmitry Shechtman <damnian@gmail.com>
2015-11-18 19:22:03 +0200

    Add FolderIndexer

---
+++ amdl2maml/Converter/FolderIndexer.cs
@@ -0,0 +1,108 @@
+﻿using PCLStorage;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Amdl.Maml.Converter
+{
+    /// <summary>
+    /// Topic folder converter.
+    /// </summary>
+    public class FolderIndexer
+    {
+        #region Static Members
+
+        private static readonly string[] Extensions = new[] { ".md", ".markdown", ".amdl" };
+
+        /// <summary>
+        /// Indexes the specified input folder.
+        /// </summary>
+        /// <param name="path">Base path.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>Topic data.</returns>
+        public static async Task<IEnumerable<TopicData>> IndexAsync(string path, CancellationToken cancellationToken)
+        {
+            return await IndexAsync(path, string.Empty, cancellationToken);
+        }
+
+        private static Task<IEnumerable<TopicData>> IndexAsync(string path, string relativePath, CancellationToken cancellationToken)
+        {
+            var converter = new FolderIndexer(path, relativePath);
+            return converter.IndexAsync(cancellationToken);
+        }
+
+        private static bool IsTopic(IFolder folder)
+        {
+            return !folder.Name.StartsWith(".");
+        }
+
+        private static bool IsTopic(IFile file)
+        {
+            if (file.Name.StartsWith("."))
+                return false;
+            var extension = Path.GetExtension(file.Name);
+            return Extensions.Contains(extension, StringComparer.OrdinalIgnoreCase);
+        }
+
+        #endregion
+
+        private readonly string path;
+        private readonly string relativePath;
+        private readonly string folderName;
+
+        private FolderIndexer(string path, string relativePath)
+        {
+            this.path = path;
+            this.relativePath = relativePath;
+            if (relativePath != null)
+                this.folderName = Path.GetFileName(relativePath);
+        }
+
+        private async Task<IEnumerable<TopicData>> IndexAsync(CancellationToken cancellationToken)
+        {
+            var folder = await FileSystem.Current.GetFolderFromPathAsync(this.path, cancellationToken);
+            var topics = await IndexFilesAsync(folder, cancellationToken);
+            var subfolderTopics = await IndexFoldersAsync(folder, cancellationToken);
+            return topics.Concat(subfolderTopics);
+        }
+
+        private async Task<IEnumerable<TopicData>> IndexFoldersAsync(IFolder folder, CancellationToken cancellationToken)
+        {
+            var folders = await folder.GetFoldersAsync(cancellationToken);
+            var folderTopics = folders.Where(IsTopic)
+                .Select(f => IndexFolderAsync(f, relativePath, cancellationToken));
+            var topicsMany = await Task.WhenAll(folderTopics);
+            return topicsMany.SelectMany(t => t);
+        }
+
+        private static Task<IEnumerable<TopicData>> IndexFolderAsync(IFolder folder, string relativePath, CancellationToken cancellationToken)
+        {
+            relativePath = Path.Combine(relativePath, folder.Name);
+            return IndexAsync(folder.Path, relativePath, cancellationToken);
+        }
+
+        private async Task<IEnumerable<TopicData>> IndexFilesAsync(IFolder folder, CancellationToken cancellationToken)
+        {
+            var files = await folder.GetFilesAsync(cancellationToken);
+            return files.Where(IsTopic)
+                .Select(CreateTopicData);
+        }
+
+        private TopicData CreateTopicData(IFile file)
+        {
+            var name = Path.GetFileNameWithoutExtension(file.Name);
+            var type = GetTopicType(name);
+            return new TopicData(name, type, relativePath);
+        }
+
+        private TopicType GetTopicType(string name)
+        {
+            if (name.Equals(folderName, StringComparison.OrdinalIgnoreCase))
+                return TopicType.Orientation;
+            return TopicType.Conceptual;
+        }
+    }
+}
--- amdl2maml/Converter/TopicData.cs
+++ amdl2maml/Converter/TopicData.cs
@@ -20,9 +20,13 @@ namespace Amdl.Maml.Converter
         /// Creates a new instance of the <see cref="TopicData"/> class.
         /// </summary>
         /// <param name="name">Topic name.</param>
-        public TopicData(string name)
+        /// <param name="type">Topic type.</param>
+        /// <param name="relativePath">Relative path to the topic file.</param>
+        public TopicData(string name, TopicType type, string relativePath)
         {
             Name = name;
+            Type = type;
+            RelativePath = relativePath;
         }
 
         /// <summary>
@@ -41,7 +45,7 @@ namespace Amdl.Maml.Converter
         /// Gets or sets the type.
         /// </summary>
         /// <value>Topic Type.</value>
-        public TopicType Type { get; set; }
+        public TopicType Type { get; private set; }
 
         /// <summary>
         /// Gets the title.
@@ -53,6 +57,6 @@ namespace Amdl.Maml.Converter
         /// Gets or sets the relative path.
         /// </summary>
         /// <value>Relative path.</value>
-        public string RelativePath { get; set; }
+        public string RelativePath { get; private set; }
     }
 }
