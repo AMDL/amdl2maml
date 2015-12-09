28d04e25f27e8b60e9cb3abfc03d72b21a818cd8
Dmitry Shechtman <damnian@gmail.com>
2015-12-09 04:51:51 +0200

    Add Step.cs

---
+++ amdl2maml/Converter/Step.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Amdl.Maml.Converter
+{
+    class Step<T>
+    {
+        private readonly string title;
+        private readonly Func<T, Task<T>> taskFactory;
+
+        public Step(string title, Func<T, Task<T>> taskFactory)
+        {
+            this.title = title;
+            this.taskFactory = taskFactory;
+        }
+
+        private async Task<T> RunAsync(T data, IProgress<Indicator> progress, int index, int count)
+        {
+            Indicator.Report(progress, count, index, title);
+            return await taskFactory(data);
+        }
+
+        public static async Task<T> RunAllAsync(T data, IEnumerable<Step<T>> steps, IProgress<Indicator> progress)
+        {
+            var stepsArray = steps.ToArray();
+            var count = stepsArray.Length;
+            for (int index = 0; index < count; index++)
+                data = await stepsArray[index].RunAsync(data, progress, index, count);
+            return data;
+        }
+    }
+}
