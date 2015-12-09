b2fade0d7286817139363bf4b62d99cc22577422
Dmitry Shechtman <damnian@gmail.com>
2015-12-09 04:42:46 +0200

    Add XML doc to Verbosity

--- amdl2maml/Converter.Console/Verbosity.cs
+++ amdl2maml/Converter.Console/Verbosity.cs
@@ -2,20 +2,38 @@
 
 namespace Amdl.Maml.Converter.Console
 {
+    /// <summary>
+    /// Verbosity.
+    /// </summary>
     enum Verbosity
     {
+        /// <summary>
+        /// No output.
+        /// </summary>
         [Description("No output")]
         Silent,
 
+        /// <summary>
+        /// Minimal output.
+        /// </summary>
         [Description("Minimal output")]
         Minimal,
 
+        /// <summary>
+        /// Normal output.
+        /// </summary>
         [Description("Normal output")]
         Normal,
 
+        /// <summary>
+        /// Detailed output.
+        /// </summary>
         [Description("Detailed output")]
         Detailed,
 
+        /// <summary>
+        /// Very detailed output.
+        /// </summary>
         [Description("Very detailed output")]
         Insane,
     }
